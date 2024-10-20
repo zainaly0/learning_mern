0640ab6b422e8081fb905ae288deeb0402970197
Zaid Aly <zaidk4076@gmail.com>
2024-10-20 22:42:01 +0530

    first commit

---
+++ week1/1.index.js
@@ -0,0 +1,26 @@
+// let fname = prompt("enter your first name");
+// let lname = prompt("enter your last name");
+// console.log('good morning ', fname+ lname);
+// let gender = prompt("enter your gender");
+// if(gender = 'male'){
+//     console.log('male good morning ', fname+ lname);
+// }else{
+//     console.log('female good morning ', fname+ lname);
+// }
+
+
+
+// let count = 0;
+// for(a = 0; a<1000; a++){
+//     count = count+a;
+// }
+// console.log(count)
+
+
+let name = "zaid";
+let age = 23;
+let isMarried = true;
+
+console.log('this person name is: ' + name+  " and their age " + age)
+console.log(name + (isMarried? " is married ": " not married"));
+
